cb8752508c7878a4151da5619b2bd7723d8aca23
CalvinGaiennie <calvingaiennie@gmail.com>
2025-09-15 22:19:10 -0500

    updated cors to allow prod frontend

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://cghabittracker.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

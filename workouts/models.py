763cce7407ee7895067f5fe752f32085f1fdc4e9
CalvinGaiennie <calvingaiennie@gmail.com>
2025-10-14 20:20:56 -0500

    fixed a couple errors related to interacting with the workout table

--- workouts/models.py
+++ workouts/models.py
@@ -3,13 +3,12 @@ from db.session import Base
 
 
 class Workout(Base):
-    __tablename__ = "workouts"
+    __tablename__ = "workout"
     
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     started_at = Column(TIMESTAMP, server_default=text("now()"))
     ended_at = Column(TIMESTAMP, nullable=True)
     title = Column(String(255))
-    workout_types = Column(ARRAY(String), nullable=True)  # Array of workout types
+    workout_type = Column(ARRAY(String), nullable=True)  # Array of workout types
     notes = Column(String, nullable=True)
-    exercises = Column(JSON, nullable=True)  # Store exercise data as JSON
